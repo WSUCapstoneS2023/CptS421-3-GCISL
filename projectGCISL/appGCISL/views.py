c845e33254235a49fcddc67075c3872a594b56e7
lukethorvilson <luke.thorvilson@wsu.edu>
2023-03-07 15:16:52 -0800

    Base view functions for register and login.

--- projectGCISL/appGCISL/views.py
+++ projectGCISL/appGCISL/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+import datetime
+
 
 # Create your views here.
+# Register
+def register(request, user):
+    pass
+
+
+# Login
+def login(request, username, password):
+    pass
