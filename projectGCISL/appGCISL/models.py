d52404256a113cf1f189dd416284bb68011c1c07
lukethorvilson <luke.thorvilson@wsu.edu>
2023-03-06 22:03:18 -0800

    Created baseline user models for user functionality/ such as login, and register. Base user class takes fields according to ER diagram. This in which when a user is created we use the UserManager to create the users. To check user type check if user is factuly through property functions.

--- projectGCISL/appGCISL/models.py
+++ projectGCISL/appGCISL/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+
+# user model, with all fields neccessary for first milestone
+class UserManager(BaseUserManager):
+    def create_Resident(self, firstname, lastname, phonenum, email, password=None):
+        if not email:
+            raise ValueError('Resident must have an email address.')
+        if not firstname:
+            raise ValueError('Resident must have first name.')
+        if not lastname:
+            raise ValueError('Resident must have last name.')
+        if not phonenum:
+            raise ValueError('Resident must have phone number.')
+        
+        user = self.model(
+            first_name=firstname,
+            last_name=lastname,
+            phone=phonenum,
+            email=self.normalize_email(email),
+        )
+        user.resident = True
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_Faculty(self, firstname, lastname, phonenum, email, password=None):
+        if not email:
+            raise ValueError('Faculty must have an email address.')
+        if not firstname:
+            raise ValueError('Faculty must have first name.')
+        if not lastname:
+            raise ValueError('Faculty must have last name.')
+        
+        user = self.model(
+            first_name=firstname,
+            last_name=lastname,
+            phone=phonenum,
+            email=self.normalize_email(email),
+        )
+        user.faculty = True
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=30, unique=True)
+    first_name = models.CharField(verbose_name= "first", max_length=30)
+    last_name = models.CharField(verbose_name= "last", max_length=30)
+    phone = models.CharField(unique= True, max_length=10, help_text="Enter phone number, example: 1234567890")
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    faculty = models.BooleanField(default=False)
+    resident = models.BooleanField(default=False)
+
+    #possibly use this for admin too?
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first', 'last', 'email']
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+    
+    # checks for whether the user is resident, returns false if not
+    @property
+    def is_Resident(self):
+        return self.resident
+    
+    # checks for whether the user is faculty, returns false if not
+    @property
+    def is_Faculty(self):
+        return self.faculty
--- projectGCISL/projectGCISL/settings.py
+++ projectGCISL/projectGCISL/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'projectGCISL.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
