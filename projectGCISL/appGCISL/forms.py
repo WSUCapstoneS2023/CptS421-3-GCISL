08632b2842f49cbbde5431a23841b79460648b3f
lukethorvilson <luke.thorvilson@wsu.edu>
2023-03-24 14:37:25 -0700

    Changes to forms, specifically register form. Also made changes to register view functionality.

--- projectGCISL/appGCISL/forms.py
+++ projectGCISL/appGCISL/forms.py
@@ -3,12 +3,32 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import GCISLUser
 
 # create your forms here
+# followed implementation from 
 class RegistrationForm(UserCreationForm):
+    password1 = forms.CharField(label=("Password"),
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(label=("Password confirmation"),
+        widget=forms.PasswordInput,
+        help_text=("Enter the same password as above, for verification."))
+    
     class Meta:
         model = GCISLUser
-        fields = ['email', 'first_name', 'last_name', 'phone']
+        fields = ['email', 'first_name', 'last_name', 'username', 'age_range', 'location']
+    
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
 
-class LoginForm(forms.Form):
-    username = forms.EmailField(max_length=60, help_text="Please enter username/email.")
-    password = forms.CharField(help_text="Please enter email/username to login.")
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
     
--- projectGCISL/projectGCISL/urls.py
+++ projectGCISL/projectGCISL/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from appGCISL import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.landing_view, name='landing'),
