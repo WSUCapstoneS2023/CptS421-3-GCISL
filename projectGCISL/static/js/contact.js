2182b5edeafb8facfe674be4f88e8746fa24cd46
AliDzulfiqar <66514385+AliDzulfiqar@users.noreply.github.com>
2023-04-17 22:28:58 -0700

    added contact funct

---
+++ projectGCISL/static/js/contact.js
@@ -0,0 +1,22 @@
+function contact(event) {
+    event.preventDefault()
+    // const loading = document.querySelector('.modal__overlay--loading')
+    // const success = document.querySelector('.modal__overlay--success')
+    // loading.classList += " modal__overlay--visible"
+    
+    emailjs
+        .sendForm(
+            'service_wp048se',
+            'template_si8f8fp',
+            event.target,
+            'IoiwiKiSBN_lWY9MM'
+        ).then(() => {
+            // loading.classList.remove("modal__overlay--visible")
+            setTimeout(3000)
+        }).catch(() => {
+            // loading.classList.remove("modal__overlay--visible")
+            alert(
+                "The email service is temporatily unavailable. Please contact me directly on malidzulfiqar@gmail.com"
+            )
+        }) 
+}
