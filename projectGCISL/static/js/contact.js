9099c44d5af8fc421d25f65aa614ae6ca8259348
AliDzulfiqar <66514385+AliDzulfiqar@users.noreply.github.com>
2023-04-18 02:53:16 -0700

    added save state

--- projectGCISL/static/js/contact.js
+++ projectGCISL/static/js/contact.js
@@ -1,22 +1,24 @@
 function contact(event) {
-    event.preventDefault()
-    const loading = document.querySelector('.contact__overlay--loading')
-    const success = document.querySelector('.contact__overlay--success')
-    loading.classList += " contact__overlay--visible"
-    
-    emailjs
-        .sendForm(
-            'service_wp048se',
-            'template_si8f8fp',
-            event.target,
-            'IoiwiKiSBN_lWY9MM'
-        ).then(() => {
-            loading.classList.remove("contact__overlay--visible")
-            setTimeout(success.classList += " contact__overlay--visible", 3000)
-        }).catch(() => {
-            loading.classList.remove("contact__overlay--visible")
-            alert(
-                "The email service is temporatily unavailable. Please contact me directly on malidzulfiqar@gmail.com"
-            )
-        }) 
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_wp048se",
+      "template_si8f8fp",
+      event.target,
+      "IoiwiKiSBN_lWY9MM"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      setTimeout((success.classList += " modal__overlay--visible"), 3000);
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporatily unavailable. Please contact me directly on malidzulfiqar@gmail.com"
+      );
+    });
 }
