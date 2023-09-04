646916097099c2d7f5d9f4db78d034bbb1ce03c0
kblue <kristenleanne.blue@wsu.edu>
2023-09-03 19:52:13 -0700

    Adding database connection. Migration not yet done.

--- projectGCISL/projectGCISL/settings.py
+++ projectGCISL/projectGCISL/settings.py
@@ -102,7 +102,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'survey': {
+        'NAME': 'survey',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'USER': 'postgres',
+        'PASSWORD': 'test123',
+    },   
 }
 
 # Password validation
