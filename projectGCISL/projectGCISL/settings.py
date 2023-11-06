d5870e7d0f1810101e759032e63e35043a66f65a
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-11-06 12:05:07 -0800

    Hide pass

--- projectGCISL/projectGCISL/settings.py
+++ projectGCISL/projectGCISL/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'LocalDBGCI',
         'USER': 'postgres',
-        'PASSWORD': 'Amat3rasu',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': 5432,
     }
