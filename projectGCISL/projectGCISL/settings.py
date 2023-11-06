b6e138950b1b4d23a3d4a5631525550e95b15065
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-11-06 12:03:08 -0800

    Localdb pass

--- projectGCISL/projectGCISL/settings.py
+++ projectGCISL/projectGCISL/settings.py
@@ -106,16 +106,28 @@ WSGI_APPLICATION = 'projectGCISL.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#          'ENGINE': 'django.db.backends.postgresql',
+#          'NAME': 'iakvhaso',
+#          'USER' : 'iakvhaso',
+#          'PASSWORD': '3aXyp7AwTBHNVElrCP00AGcvBVM2FWHf',
+#          'HOST': 'jelani.db.elephantsql.com',
+#          'PORT' : '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': 'iakvhaso',
-         'USER' : 'iakvhaso',
-         'PASSWORD': '3aXyp7AwTBHNVElrCP00AGcvBVM2FWHf',
-         'HOST': 'jelani.db.elephantsql.com',
-         'PORT' : '5432'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'LocalDBGCI',
+        'USER': 'postgres',
+        'PASSWORD': 'Amat3rasu',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
