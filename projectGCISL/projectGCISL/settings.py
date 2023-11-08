2ba03bd55666637de12fc4cf595b589eb24cdfe9
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-11-08 00:06:26 -0800

    Updated staticfiles setting

--- projectGCISL/projectGCISL/settings.py
+++ projectGCISL/projectGCISL/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import os 
+import os
 
 from pathlib import Path
 
@@ -193,12 +193,10 @@ if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+        BASE_DIR / 'static'
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
