e03b88217ac7e9b832109772c376bd04b2cc4767
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-11-08 00:02:20 -0800

    Updated staticfiles setting

--- projectGCISL/build.sh
+++ projectGCISL/build.sh
@@ -1,4 +1,4 @@
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic
+python manage.py collectstatic --no-input
 python manage.py migrate
