ba3b9eb45ae0de154e43998f93410727a95d62b3
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-11-07 23:56:08 -0800

    Updated staticfiles setting:

--- projectGCISL/projectGCISL/settings.py
+++ projectGCISL/projectGCISL/settings.py
@@ -196,10 +196,3 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-
-STATIC_ROOT = BASE_DIR / 'static-cdn'
