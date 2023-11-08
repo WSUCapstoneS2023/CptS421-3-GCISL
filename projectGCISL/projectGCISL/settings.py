45071903f0afc4c058290f6fbceb878376b5420a
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-11-08 15:22:38 -0800

    Changed static settings

--- projectGCISL/projectGCISL/settings.py
+++ projectGCISL/projectGCISL/settings.py
@@ -152,6 +152,9 @@ STATIC_URL = '/static/'
 
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -159,10 +162,6 @@ if not DEBUG:
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-    STATICFILES_DIRS = [
-        BASE_DIR / 'static'
-    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
