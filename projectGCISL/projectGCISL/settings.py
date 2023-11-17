43db9b0ebbe6637bd5a66d208f45358b28226521
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-11-17 11:10:26 -0800

    Removed pw in settings

--- projectGCISL/projectGCISL/settings.py
+++ projectGCISL/projectGCISL/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'LocalDBGCI',
         'USER': 'postgres',
-        'PASSWORD': 'Amat3rasu',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': 5432,
     }
