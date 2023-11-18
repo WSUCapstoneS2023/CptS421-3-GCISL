3bf79cb0861ec266be66806c7bae873c4521bbba
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-11-18 00:29:26 -0800

    Edit build script

--- projectGCISL/build.sh
+++ projectGCISL/build.sh
@@ -1,4 +1,5 @@
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations appGCISL
 python manage.py migrate
