f98cc797b95fe503ebb8234f5fee414880670897
AliDzulfiqar <m.dzulfiqar@wsu.edu>
2023-11-18 00:15:37 -0800

    Added host

--- projectGCISL/projectGCISL/settings.py
+++ projectGCISL/projectGCISL/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
